b2575388704a22bd00953fcf141a51dc20959604
meeplabsdev <76091175+meeplabsdev@users.noreply.github.com>
2025-12-22 18:06:01 +0000

    misc: Update example to show usage of the buttons on the sprig.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <pico/cyw43_arch.h>
 #include <pico/bootrom.h>
 
+#include <algorithm>
+
 #include "hardware.h"
 
 #define FOREVER while (1)
@@ -9,7 +11,7 @@
 struct repeating_timer _bootsel_task;
 bool bootsel_task(struct repeating_timer *rt)
 {
-    if (get_button(BOOTSEL))
+    if (get_button(BUTTON::BOOTSEL))
     {
         reset_usb_boot(0, 0);
         return false;
@@ -18,35 +20,40 @@ bool bootsel_task(struct repeating_timer *rt)
     return true;
 }
 
+float l_power = 0.0;
+float r_power = 0.0;
+float delta = 0.1;
+
+struct repeating_timer _main_task;
+bool main_task(struct repeating_timer *rt)
+{
+    set_led(LED::STATUS, l_power);
+    set_led(LED::NETWORK, r_power);
+
+    if (get_button(BUTTON::W))
+        l_power = std::clamp(l_power + delta, 0.0f, 1.0f);
+    else if (get_button(BUTTON::S))
+        l_power = std::clamp(l_power - delta, 0.0f, 1.0f);
+
+    if (get_button(BUTTON::I))
+        r_power = std::clamp(r_power + delta, 0.0f, 1.0f);
+    else if (get_button(BUTTON::K))
+        r_power = std::clamp(r_power - delta, 0.0f, 1.0f);
+
+    return true;
+}
+
 int main()
 {
-    // init
-    stdio_init_all();
-    if (!hardware_init_all())
+    if (!stdio_init_all() ||
+        !hardware_init_all())
     {
         printf("Init failed");
         return -1;
     }
 
-    // tasks
-    add_repeating_timer_ms(20, bootsel_task, NULL, &_bootsel_task);
+    add_repeating_timer_ms(100, bootsel_task, NULL, &_bootsel_task);
+    add_repeating_timer_ms(10, main_task, NULL, &_main_task);
 
-    // main
-    set_led(STATUS, 1);
-
-    float power = 0.0;
-    float delta = 0.1;
-
-    FOREVER
-    {
-        sleep_ms(10);
-        set_led(NETWORK, power);
-
-        power += delta;
-        if (power >= 1.0 || power <= 0.0)
-        {
-            delta = -delta;
-            sleep_ms(500);
-        }
-    }
+    FOREVER tight_loop_contents();
 }
