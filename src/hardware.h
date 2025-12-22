3e170bae8ab7dda8530ee36cc03f7914cb94ef56
meeplabsdev <76091175+meeplabsdev@users.noreply.github.com>
2025-12-22 21:09:22 +0000

    feat: Add code to enable interaction with the screen on the sprig

--- src/hardware.h
+++ src/hardware.h
@@ -1,6 +1,7 @@
 #include "hardware/button.h"
 #include "hardware/led.h"
-#include "hardware/screen.h"
+#include "hardware/screen_tft.h"
+#include <functional>
 
 #ifndef __HARDWARE__
 #define __HARDWARE__
@@ -28,9 +29,6 @@ enum BUTTON
 bool initialized = false;
 bool hardware_init_all()
 {
-    init_led_sided(LED_L);
-    init_led_sided(LED_R);
-
     init_button(BUTTON_W);
     init_button(BUTTON_A);
     init_button(BUTTON_S);
@@ -40,8 +38,13 @@ bool hardware_init_all()
     init_button(BUTTON_K);
     init_button(BUTTON_L);
 
+    init_led_sided(LED_L);
+    init_led_sided(LED_R);
+
     bool success =
         init_led_builtin();
+
+    init_screen_tft();
 
     initialized = true;
     return success;
@@ -77,6 +80,40 @@ void set_led(enum LED led, float power)
         set_led_sided(LED_R, power);
     else
         set_led_builtin(power);
+}
+
+void draw_pixel(uint16_t colour)
+{
+    fill_start();
+    fill_send(colour);
+    fill_finish();
+}
+
+void draw_callback(std::function<uint8_t(int, int)> callback, int x0, int y0, int x1, int y1)
+{
+    fill_start();
+    // left to right, top to bottom
+    for (int x = x0; x < x1; x++)
+    {
+        for (int y = y0; y < y1; y++)
+        {
+            fill_send(callback(x, y));
+        }
+    }
+    fill_finish();
+}
+
+void draw_rectangle(uint16_t colour, int x0, int y0, int x1, int y1)
+{
+    draw_callback(
+        [colour](int x, int y)
+        { return colour; },
+        x0, y0, x1, y1);
+}
+
+void draw_screen(uint16_t colour)
+{
+    draw_rectangle(colour, 0, 0, 160, 128);
 }
 
 #endif
