f7399fc18869e528d831fba34010d9a37fa9896b
meeplabsdev <76091175+meeplabsdev@users.noreply.github.com>
2026-01-17 00:24:42 +0000

    feat: Update badapple example

--- examples/badapple/main.cpp
+++ examples/badapple/main.cpp
@@ -1,28 +1,35 @@
 #include <pico/cyw43_arch.h>
+#include <pico/stdio.h>
 #include <screen.h>
 #include <speaker.h>
 #include <storage.h>
 
 #define FOREVER while (1)
 
-static Screen screen;
-static Speaker speaker;
-static Storage storage;
+struct task_args {
+  Screen *screen;
+  Speaker *speaker;
+  Storage *storage;
+  FIL *file;
+};
 
 struct repeating_timer _screen_task;
 bool screen_task(repeating_timer_t *rt) {
-  screen.blit();
+  task_args *args = (task_args *)rt->user_data;
+  args->screen->blit();
   return true;
 }
 
-static FIL file;
 struct repeating_timer _main_task;
 bool main_task(repeating_timer_t *rt) {
   // read straigt into the screen buffer
-  FRESULT fr = storage.raw_read(&file, screen.screen_buf, FRAME_SIZE * 2);
+  task_args *args = (task_args *)rt->user_data;
+  FRESULT fr = args->storage->raw_read(args->file, args->screen->screen_buf,
+                                       FRAME_SIZE * 2);
 
   if (fr != FR_OK) {
-    f_close(&file);
+    args->screen->draw_text(RGB(255, 100, 100), std::to_string(fr), 6, 6);
+    f_close(args->file);
     return false;
   }
 
@@ -30,11 +37,29 @@ bool main_task(repeating_timer_t *rt) {
 }
 
 int main() {
-  screen = Screen();
-  speaker = Speaker();
-  storage = Storage();
+  Screen screen = Screen();
+  Speaker speaker = Speaker();
+  Storage storage = Storage();
 
-  add_repeating_timer_ms(-82, screen_task, NULL, &_screen_task);
+  FRESULT fr_mount = storage.mount();
+  if (!storage.is_ready()) {
+    screen.draw_text(RGB(200, 200, 200), "NO_SD", 6, 110);
+    screen.blit();
+
+    return -1;
+  }
+
+  FIL file;
+  f_open(&file, "badapple.bin", FA_READ);
+
+  task_args args = {
+      &screen,
+      &speaker,
+      &storage,
+      &file,
+  };
+
+  add_repeating_timer_ms(-82, screen_task, &args, &_screen_task);
 
   storage.read("logo.bin", screen.screen_buf, FRAME_SIZE * 2, 0);
   screen.set_backlight(true);
@@ -42,9 +67,7 @@ int main() {
 
   sleep_ms(800);
 
-  FIL badapple_file;
-  f_open(&badapple_file, "badapple.bin", FA_READ);
-  add_repeating_timer_ms(-50, main_task, NULL, &_main_task);
+  add_repeating_timer_ms(-50, main_task, &args, &_main_task);
 
   FOREVER tight_loop_contents();
 }
