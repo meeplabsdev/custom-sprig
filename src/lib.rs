a12414a17316c71f82ee86d94e96e8915489a9aa
meeplabsdev <76091175+meeplabsdev@users.noreply.github.com>
2026-01-25 01:04:01 +0000

    feat: Add C bridge for buttons

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@
 #![cfg(not(test))]
 
 mod bridge;
-use crate::bridge::led_bridge;
+use crate::bridge::button_bridge::*;
+use crate::bridge::led_bridge::*;
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
@@ -13,9 +14,12 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 #[unsafe(no_mangle)]
 pub extern "C" fn main() -> ! {
     unsafe {
-        let led: *mut led_bridge::LED = led_bridge::LED_new(led_bridge::LED_TYPE::STATUS, true);
-        led_bridge::LED_set_brightness_bool(led, true);
-    }
+        let led: *mut LED = LED_new(LED_TYPE::STATUS, true);
+        let button: *mut Button = Button_new(BUTTON_TYPE::L_LEFT);
 
-    loop {}
+        loop {
+            let pressed = Button_is_pressed(button);
+            LED_set_brightness_bool(led, pressed);
+        }
+    }
 }
